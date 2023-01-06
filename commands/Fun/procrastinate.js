672b40a19b381ecd5d6acb99a9b655bea4f0ebeb
Peace <63497055+AestheticsPeace@users.noreply.github.com>
2023-01-06 11:37:07 +0000

    Update procrastinate.js
    lols

--- commands/Fun/procrastinate.js
+++ commands/Fun/procrastinate.js
@@ -7,93 +7,18 @@ module.exports = {
      * @param {Message} msg
      */
 	async execute(msg, args) { 
-		// time
-		let t1 = Date.now()
-		await msg.fetch()
+
+        function randomHexColor() {
+            return '#' + ('000000' + Math.floor(Math.random() * 16777215).toString(16)).slice(-6)
+        };
+
+        let procrastinateWords = ["I can't do it", "Don't want to", "Sure", "I'm not sure?", "no.", "Don't ask me"]
 		let m = await msg.channel.send(new MessageEmbed().setDescription('Yes').setColor('GREEN'))
-		m.edit(
-			new MessageEmbed()
-				.setDescription([
-					`No`,
-				].join('\n'))
-				.setColor('RED')
-        )
-        m.edit(
-         new MessageEmbed()
-				.setDescription([
-					`Maybe`,
-				].join('\n'))
-                .setColor('PURPLE')
-        )
-        m.edit(
-            new MessageEmbed()
-                   .setDescription([
-                       `I DON'T KNOW`,
-                   ].join('\n'))
-                   .setColor('BLACK')
-        )
-        m.edit(
-            new MessageEmbed()
-                   .setDescription([
-                       `DON'T ASK ME`,
-                   ].join('\n'))
-                   .setColor('PINK')
-                )
-         m.edit(
-            new MessageEmbed()
-                     .setDescription([
-                         `No`,
-                     ].join('\n'))
-                     .setColor('RED')
-             )
-         m.edit(
-            new MessageEmbed()
-                       .setDescription([
-                          `Maybe`,
-                      ].join('\n'))
-                       .setColor('PURPLE')
-               )
-         m.edit(
-            new MessageEmbed()
-                       .setDescription([
-                          `I DON'T KNOW`,
-                      ].join('\n'))
-                       .setColor('BLACK')
-                )
-          m.edit(
-             new MessageEmbed()
-                       .setDescription([
-                          `DON'T ASK ME`,
-                      ].join('\n'))
-                       .setColor('PINK')
-                )
-           m.edit(
-             new MessageEmbed()
-                       .setDescription([
-                          `No`,
-                      ].join('\n'))
-                       .setColor('RED')
-                 )
-           m.edit(
-             new MessageEmbed()
-                       .setDescription([
-                          `Maybe`,
-                      ].join('\n'))
-                       .setColor('PURPLE')
-                )
-            m.edit(
-              new MessageEmbed()
-                       .setDescription([
-                          `I DON'T KNOW`,
-                      ].join('\n'))
-                       .setColor('BLACK')
-                )
-            m.edit(
-              new MessageEmbed()
-                       .setDescription([
-                         `DON'T ASK ME`,
-                      ].join('\n'))
-                       .setColor('PINK')
-                )
+
+        for(let idx = 0; idx < 10; idx++) {
+            setTimeout(async function() {
+                m.edit(new MessageEmbed().setColor(randomHexColor()).setDescription(procrastinateWords[Math.floor(Math.random() * procrastinateWords.length)]))
+            }, 1000)
+        }
 	},
 };
